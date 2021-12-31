2813580ce7bc6fd755a4103645dc00aa919acc78
hoangviet2 <hoangviet18306@gmail.com>
2021-12-31 18:03:27 +0700

    pow function

---
+++ Labatory/Pow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+double pow(double base, int exp){
+    double ans = 1;
+    if(exp<=10){
+        for(int i=0;i<exp;++i){
+            ans *= base;
+        }
+    }
+}
+int main(){
+    
+    return 0;
+}
